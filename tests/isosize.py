255582b0a8e52132f193c8fe22892ebed868cc5c
sanjayraj32 <167397207+sanjayraj32@users.noreply.github.com>
2024-04-23 15:21:21 +0530

    Update isosize.py
    isosize working version

--- tests/isosize.py
+++ tests/isosize.py
@@ -1,19 +1,14 @@
-from basetest import BaseTest
-from testapi import get_var
-import autotest
+import os
+from testapi import *
 
-class IsoSizeTest(BaseTest):
-    def run(self):
-        iso = get_var("ISO")
-        size = os.path.getsize(iso)
-        result = 'ok' if size <= get_var("ISO_MAXSIZE") else 'fail'
-        autotest.bmwqemu.diag(f"Check if actual ISO size {size} fits {get_var('ISO_MAXSIZE')}: {result}")
-        self.result(result)
 
-    @staticmethod
-    def test_flags():
-        return { 'important': 1 }
+def run(self):
+    iso = get_var("ISO")
+    size = os.path.getsize(iso)
+    iso_maxsize = int(get_var("ISO_MAXSIZE"))
+    result = 'ok' if size <= iso_maxsize else 'fail'
 
-if __name__ == "__main__":
-    test = IsoSizeTest()
-    test.run()
+        # autotest.bmwqemu.diag(f"Check if actual ISO size {size} fits {get_var('ISO_MAXSIZE')}: {result}")
+
+def test_flags(self):
+    return { 'important': 1 }
